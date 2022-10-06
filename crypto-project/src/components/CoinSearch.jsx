a7b9608f0be3575b93d3c932fa6af5cefbd10926
Linh777GKOG <97266612+Linh777GKOG@users.noreply.github.com>
2022-10-06 15:01:19 +0700

    cm msg

--- crypto-project/src/components/CoinSearch.jsx
+++ crypto-project/src/components/CoinSearch.jsx
@@ -7,7 +7,7 @@ const CoinSearch = ({ coins }) => {
   return (
     <div className="rounded-div my-4">
       <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
-        <h1 className="text-2xl font-bold my-2">Search Crypto</h1>
+        <h1 className="text-2xl font-bold my-2">Searchs Crypto</h1>
         <form>
           <input
             onChange={(e) => setSearchText(e.target.value)}
